b1b074112d7ff2f311dd85f5fe82d08f8bd35c5a
Ying Yang <yingyang9416@gmail.com>
2017-11-13 21:29:20 -0500

    finished auth flow 2

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js - don't commit this !!!
+module.exports = {
+  googleClientID:
+    '181730073815-gbdt3ou9v6995ofmc3g7llplh1cguu1i.apps.googleusercontent.com',
+  googleClientSecret: 'kqw3PhWEHTb4EYB-kU7O45kH',
+  mongoURI:
+    'mongodb://yingyang:Yy19940106@ds259305.mlab.com:59305/emaily-group',
+  cookieKey: 'gfhcfxzdfghrsesdgffxfzdfgh'
+};
---
+++ config/prod.js
@@ -0,0 +1,9 @@
+// production keys here -- DO commit this
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+mongodb://yingyangprod:Yy19940106@ds259245.mlab.com:59245/emaily-group-prod
