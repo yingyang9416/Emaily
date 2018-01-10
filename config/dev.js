0e22b8f7c153c2c020da18d739b7bbd9c2d9b170
Ying Yang <yingyang9416@gmail.com>
2018-01-09 20:35:40 -0500

    Added billing and client side app

--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,7 @@ module.exports = {
   googleClientSecret: 'kqw3PhWEHTb4EYB-kU7O45kH',
   mongoURI:
     'mongodb://yingyang:Yy19940106@ds259305.mlab.com:59305/emaily-group',
-  cookieKey: 'gfhcfxzdfghrsesdgffxfzdfgh'
+  cookieKey: 'gfhcfxzdfghrsesdgffxfzdfgh',
+  stripePublishableKey: 'pk_test_2809K6ulbyikUqysqzMi2LGO',
+  stripeSecretKey: 'sk_test_E3G9JTcFtQQcDVpFbuC8zKNd'
 };
--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 // equal to: const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  googleID: String
+  googleID: String,
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema);
